a4a22db4e40cecc17a45a9e1144ffbe1a516e846
nmfretz <nmfretz@gmail.com>
2021-10-08 13:12:07 -0700

    update design and add contact form feature

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const PX_TO_REM = 16;
-const SCROLL_ICON_END_REM = 24; // final top of scrollDownIcon in rem
+const SCROLL_ICON_END_REM = 32.5; // final top of scrollDownIcon in rem
 
 const scrollDownIcon = document.querySelector("[data-scroll-down-icon]");
 const scrollDownText = document.querySelector("[data-scroll-down-text]");
@@ -15,3 +15,29 @@ window.addEventListener("scroll", (e) => {
 
   scrollDownText.style.opacity = -scrollOffset / 75 + 1;
 });
+
+const copyEmailElement = document.querySelector("[data-copy-email]");
+
+copyEmailElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  copyEmailToClipboard();
+  changeTooltipColor();
+});
+
+copyEmailElement.addEventListener("mouseover", (e) => {
+  copyEmailElement.dataset.tooltip = "click to copy email address";
+});
+
+function copyEmailToClipboard() {
+  const emailAddress = "nmfretz@gmail.com";
+  navigator.clipboard.writeText(emailAddress);
+  copyEmailElement.dataset.tooltip = `${emailAddress} copied to clipboard`;
+}
+
+function changeTooltipColor() {
+  const rootElement = document.querySelector(":root");
+  rootElement.style.setProperty("--tooltip-color", "#999");
+  setTimeout(() => {
+    rootElement.style.setProperty("--tooltip-color", "#333");
+  }, 150);
+}
